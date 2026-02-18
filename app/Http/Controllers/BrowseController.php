0b2b871fe802812777973407a5b9d87c204e3c00
Blurry <throwawaylin7@gmail.com>
2026-02-18 08:51:00 +0700

    add ip-inspiration-labels extension

--- app/Http/Controllers/BrowseController.php
+++ app/Http/Controllers/BrowseController.php
@@ -258,6 +258,16 @@ class BrowseController extends Controller {
             });
         }
 
+        if ($request->get('label')) {
+            if ($request->get('label') == 'none') {
+                $imageQuery->where('label', null);
+            } else if ($request->get('label') == 'all') {
+                $imageQuery->whereNotNull('label');
+            } else {
+                $imageQuery->whereJsonContains('label->label', $request->get('label'));
+            }
+        }
+
         $query->whereIn('id', $imageQuery->pluck('character_id')->toArray());
 
         if ($request->get('is_gift_art_allowed')) {
@@ -608,6 +618,16 @@ class BrowseController extends Controller {
             $imageQuery->whereHas('designers', function ($query) use ($designerUrl) {
                 $query->where('url', 'LIKE', '%'.$designerUrl.'%');
             });
+        }
+
+        if ($request->get('label')) {
+            if ($request->get('label') == 'none') {
+                $imageQuery->where('label', null);
+            } else if ($request->get('label') == 'all') {
+                $imageQuery->whereNotNull('label');
+            } else {
+                $imageQuery->whereJsonContains('label->label', $request->get('label'));
+            }
         }
 
         $query->whereIn('id', $imageQuery->pluck('character_id')->toArray());
